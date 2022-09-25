2cd611f24d546f1e5921f5c74ecd5a96efcd0adf
Your Name <fatihenescoban5@gmail.com>
2022-09-25 09:23:01 +0300

    kodlama.io demoları

---
+++ kodlama.io/reCapDemo_Classes/src/reCapDemo_Classes/FourOperations.java
@@ -0,0 +1,19 @@
+package reCapDemo_Classes;
+
+public class FourOperations {
+	public int Sum(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	public int Subtraction(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	public int Multiplication(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public int Division(int number1, int number2) {
+		return number1 / number2;
+	}
+}
---
+++ kodlama.io/reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FourOperations fourOperations = new FourOperations();
+		int crollary = fourOperations.Sum(3, 4);
+		System.out.println(crollary);
+
+	}
+}
