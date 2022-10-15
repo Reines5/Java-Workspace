9c288c60a762059059172ae7557f56749bb881c0
Fatih Enes <61228657+Reines5@users.noreply.github.com>
2022-10-15 13:25:12 +0300

    Delete kodlamaIO directory

--- kodlamaIO/src/kodlamaIO/core/logging/FileLogger.java
+++
@@ -1,9 +0,0 @@
-package kodlamaIO.core.logging;
-
-public class FileLogger implements Logger{
-	
-	@Override
-	public void log(String msg) {
-		System.out.printf("Dosyaya loglandı: ½s½n", msg);
-	}
-}
